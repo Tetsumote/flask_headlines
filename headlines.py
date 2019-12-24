5963d638109433612cb3331d9057d6172471bd9d
Paweł Grajewski <unionproject88@gmail.com>
2019-12-24 12:34:52 +0100

    start project

---
+++ headlines.py
@@ -0,0 +1,32 @@
+import feedparser
+from flask import Flask 
+
+app = Flask(__name__)
+
+RSS_FEEDS = {
+    'bbc':'http://feeds.bbci.co.uk/news/rss.xml',
+    'world':'http://feeds.bbci.co.uk/news/world/rss.xml',
+    'tech':'http://feeds.bbci.co.uk/news/science_and_environment/rss.xml',
+}
+    
+
+@app.route("/")
+@app.route("/<publication>")
+
+def get_news(publication="bbc"):
+    feed = feedparser.parse(RSS_FEEDS[publication])
+    first_article = feed['entries'][0]
+    return """
+    <html>
+    <body>
+    <h1> BBC Headlines </h1>
+    <b>{0}</b><br/>
+    <i>{1}</i><br/>
+    <p>{2}</p><br/>
+    </body>
+    </html>
+    """.format(first_article.get("title"),first_article.get("published"),first_article.get("summary"))
+
+
+if __name__ == '__main__':
+    app.run(port=5000, debug=True)
